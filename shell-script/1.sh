05fb1e437f0743fb5a9267bf2bb463880859ae96
ManikandanIAI <manikandan.arumugam@iaisolution.com>
2025-07-08 17:02:42 +0530

    modified copy.txt file

--- shell-script/1.sh
+++ shell-script/1.sh
@@ -2,11 +2,11 @@
 touch copy.txt
 #check cpu consumption
 free -m > copy.txt
-############################
+echo "############################" >> copy.txt
 #check memory consumption
 df -h > copy.txt
-#############################
+echo "#############################" >> copy.txt
 #check process taken high utilization
 ps -ef > copy.txt
-#################################
+echo "#################################" >> copy.txt
 
